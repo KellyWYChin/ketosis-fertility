6525114fcea6a666dca1a792b952ce50d94841ce
KellyWYChin <w.chin@students.uu.nl>
2022-02-28 16:16:00 +0100

    edit milk data and make a function for cumulative milk yield

--- src/Functions.R
+++ src/Functions.R
@@ -1,24 +1,11 @@
-library(roxygen2)
-roxygen2::roxygenise()
-install.packages("renv")
-renv::init()
-renv::snapshot()
-renv::restore()
-install.packages("conda")
-library(conda)
-library(docstring)
-square <- function(x){
-  #' Computes the square of the input
-  return(x^2)
+# Cumulative milk yield, mean milk yield
+cumulativeMY <- function(D){
+  df.merge %>%
+    group_by(FarmNo, CowId, CalvingDate) %>%
+    select(Lactation, DayYield, DFC) %>%
+    filter(DFC <= D) %>%
+    mutate(!!paste0('AvgMY', D) := mean(DayYield),
+           MinDFC = min(DFC),
+           MaxDFC = max(DFC),
+           !!paste0('SumMY', D) := sum(DayYield))
 }
-square
-?square
-
-square <- function(x){
-  #' Squares a number
-  #'
-  #' Provides the square of the input
-  #' @param x The value to be squared
-  return(x^2)
-}
-
