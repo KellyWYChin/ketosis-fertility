d6b3787a37fc11873c4a6878dbe142546f90b1c6
KellyWYChin <w.chin@students.uu.nl>
2022-03-02 00:32:43 +0100

    edit reproduction variables and add function to define seasons

--- src/Functions.R
+++ src/Functions.R
@@ -9,3 +9,16 @@ cumulativeMY <- function(D){
            MaxDFC = max(DFC),
            !!paste0('SumMY', D) := sum(DayYield))
 }
+
+# Define seasons
+getSeason <- function(DATES){
+  WS <- as.Date("2012-1-1", format="%Y-%m-%d")
+  SE <- as.Date("2012-4-1", format="%Y-%m-%d")
+  SS <- as.Date("2012-7-1", format="%Y-%m-%d")
+  FE <- as.Date("2012-10-1", format="%Y-%m-%d")
+
+  d <- as.Date(strftime(DATES, format="2012-%m-%d"))
+  ifelse(d >= WS & d < SE, "Winter",
+         ifelse(d >= SE & d < SS, "Spring",
+                ifelse(d >= SS & d < FE, "Summer", "Fall")))
+}
